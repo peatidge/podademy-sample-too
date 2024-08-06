ddd9731c40c2b6b9d4c3999dd79392660b4c3680
peter_m_holman <peter_m_holman@hotmail.com>
2024-08-06 14:52:22 +1000

    m

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace podademy_sample_git.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
